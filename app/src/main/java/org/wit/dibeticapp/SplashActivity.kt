4f9d10429f27488f7a1e31513112569976f95787
Joseph Bridges <joseph.bridges80@gmail.com>
2020-02-08 13:04:04 +0000

    Added in a splash screen

---
+++ app/src/main/java/org/wit/dibeticapp/SplashActivity.kt
@@ -0,0 +1,29 @@
+package org.wit.dibeticapp
+
+import androidx.appcompat.app.AppCompatActivity
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+import org.wit.dibeticapp.MainActivity
+import org.wit.dibeticapp.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash)
+
+        //3 Second Splash Time
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        },3000)
+
+    }
+}
--- app/src/main/java/org/wit/dibeticapp/SplashScreen.kt
+++
@@ -1,2 +0,0 @@
-package org.wit.dibeticapp
-
