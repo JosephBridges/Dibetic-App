a64a76545bc5de575253e5f62e071c3df131eed6
Joseph Bridges <joseph.bridges80@gmail.com>
2020-02-11 00:17:13 +0000

    Begun making the about page, v1 of it.

--- app/src/main/java/org/wit/dibeticapp/ui/about/AboutFragment.kt
+++ app/src/main/java/org/wit/dibeticapp/ui/about/AboutFragment.kt
@@ -4,29 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import org.wit.dibeticapp.R
 
-//About Page
+//1
 class AboutFragment : Fragment() {
 
-    private lateinit var slideshowViewModel: AboutViewModel
+    //2
+    companion object {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        slideshowViewModel =
-            ViewModelProviders.of(this).get(AboutViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_about, container, false)
-        val textView: TextView = root.findViewById(R.id.text_about)
-        slideshowViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
-        return root
+        fun newInstance(): AboutFragment {
+            return AboutFragment()
+        }
     }
+
+    //3
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
 }
+
